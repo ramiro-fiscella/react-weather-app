a8fd249fe358610136af6ee8b008c90a03308693
unknown <ramirofiscella@gmail.com>
2023-02-06 18:58:41 -0300

    debuggin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons'
+import Inputs from './components/Inputs'
+import TimeAndLocation from './components/TimeAndLocation'
+import TemperatureAndDetails from './components/TemperatureAndDetails'
+import Forecast from './components/Forecast'
+import getFormattedWeatherData from './services/weatherServices'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [query, setQuery] = useState({ q: 'Azul' })
+  const [units, setUnits] = useState('metric')
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data)
+      })
+    }
+
+    fetchWeather()
+  }, [query, units])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto max-w-screen-md my-4 py-5 px-32 bg-gradient-to-br from-cyan-500/80 to-blue-600/90 h-fit shadow-xl shadow-gray-900/60 rounded-md">
+      <TopButtons />
+      <Inputs />
+
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+          <Forecast title="hourly forecast" />
+          <Forecast title="daily forecast" />
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function TopButtons() {
+  const cities = [
+    {
+      id: 1,
+      title: 'Buenos Aires',
+    },
+    {
+      id: 2,
+      title: 'London',
+    },
+    {
+      id: 3,
+      title: 'Tokyo',
+    },
+    {
+      id: 4,
+      title: 'Paris',
+    },
+    {
+      id: 5,
+      title: 'Sidney',
+    },
+  ]
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button key={city.id} className="text-white text-lg font-medium">
+          {city.title}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default TopButtons
